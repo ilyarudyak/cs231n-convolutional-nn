9f984a0701ba16027e6028c0670bfd8002d39572
Ilya Rudyak <ilyarudyak@yahoo.com>
2017-10-23 01:46:32 +0300

    done analytic grad for SVM

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -28,12 +28,16 @@ def svm_loss_naive(W, X, y, reg):
   for i in xrange(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
+    classes_with_negative_margin = 0 ###
     for j in xrange(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        classes_with_negative_margin += 1 ###
+        dW[:, j] += X[i] / num_train ###        
+    dW[:, y[i]] -= classes_with_negative_margin * X[i] / num_train ###  
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
